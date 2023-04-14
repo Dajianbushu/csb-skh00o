4d6219f88fb5da63212b8e3d0deb064676c08538
Code Sandbox <hello@codesandbox.io>
2023-04-14 23:48:14 +0000

    Deployment from Codesandbox.

---
+++ index.php
@@ -0,0 +1,32 @@
+
+
+// Output the current posts title. 
+
+<?php //code here  ?>
+
+
+// Create a loop that would display 6
+// posts from the category 23.
+
+<?php //code here  ?>
+
+
+// Create a php array of 4 color names, then
+// loop trhough them and echo the each color;
+
+<?php //code here  ?>
+
+
+
+// A website has just moved hosting and
+// all pages for a custom post type are
+// returning a 404 error, what is the most likely solution?
+
+
+
+
+// How would you create a php template
+// file for a single page 
+
+
+
